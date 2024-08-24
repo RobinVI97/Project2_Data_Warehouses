ab0032a52c387c2c3e9d775e9410d0b1ded361f8
RobinVI97 <168171219+RobinVI97@users.noreply.github.com>
2024-08-24 22:49:31 +0200

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS staging_events (
     auth VARCHAR(100),
     firstName VARCHAR(100),
     gender VARCHAR(100),
-    itemInSession VARCHAR(100),
+    itemInSession INTEGER,
     lastName VARCHAR(100),
     length VARCHAR(100),
     level VARCHAR(100),
@@ -47,27 +47,27 @@ CREATE TABLE IF NOT EXISTS staging_events (
 staging_songs_table_create = ("""
 CREATE TABLE IF NOT EXISTS staging_songs (
     song_id VARCHAR(100),
-    num_songs VARCHAR(100),
+    num_songs INTEGER,
     title VARCHAR(1000),
     artist_name VARCHAR(100),
     artist_latitude VARCHAR(100),
-    year VARCHAR(100),
-    duration VARCHAR(100),
+    year INTEGER,
+    duration float,
     artist_id VARCHAR(100),
     artist_longitude VARCHAR(100),
-    artist_location VARCHAR(100),
+    artist_location VARCHAR(1000),
     PRIMARY KEY(song_id, artist_id))
     DISTSTYLE ALL;
 """)
 
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplay (
-    songplay_id INTEGER,
-    start_time DATE,
+    songplay_id INTEGER NOT NULL,
+    start_time TIMESTAMP,
     user_id INTEGER,
     level VARCHAR(100),
-    song_id INTEGER,
-    artist_id INTEGER,
+    song_id VARCHAR(100),
+    artist_id VARCHAR(100),
     session_id INTEGER,
     location VARCHAR(100),
     user_agent VARCHAR(100),
@@ -77,7 +77,7 @@ CREATE TABLE IF NOT EXISTS songplay (
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS user_table (
-    user_id INTEGER,
+    user_id INTEGER NOT NULL,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     gender VARCHAR(100),
@@ -88,18 +88,18 @@ CREATE TABLE IF NOT EXISTS user_table (
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS song (
-    song_id INTEGER,
+    song_id VARCHAR(100) NOT NULL,
     title VARCHAR(1000),
-    artist_id INTEGER,
-    year VARCHAR(100),
-    duration VARCHAR(100),
+    artist_id VARCHAR(100),
+    year INTEGER,
+    duration float,
     PRIMARY KEY(song_id))
     DISTSTYLE ALL;
 """)
 
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artist (
-    artist_id INTEGER,
+    artist_id VARCHAR(100) NOT NULL,
     name VARCHAR(100),
     location INTEGER,
     latitude VARCHAR(100),
@@ -110,13 +110,13 @@ CREATE TABLE IF NOT EXISTS artist (
 
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time (
-    start_time DATE,
-    hour VARCHAR(100),
+    start_time TIMESTAMP NOT NULL,
+    hour INTEGER,
     day INTEGER,
-    week VARCHAR(100),
-    month VARCHAR(100),
-    year VARCHAR(100),
-    weekday VARCHAR(100),
+    week INTEGER,
+    month INTEGER,
+    year INTEGER,
+    weekday INTEGER,
     PRIMARY KEY(start_time))
     DISTSTYLE ALL;
 """)
@@ -140,7 +140,8 @@ staging_songs_copy = ("""
 # FINAL TABLES
 
 songplay_table_insert = ("""
-   SELECT
+   INSERT INTO songplay (
+       SELECT
                 md5(events.sessionid || events.start_time) songplay_id,
                 events.start_time, 
                 events.userid, 
@@ -157,25 +158,32 @@ songplay_table_insert = ("""
             ON events.song = songs.title
                 AND events.artist = songs.artist_name
                 AND events.length = songs.duration
+                )
 """)
 
 user_table_insert = ("""
+    INSERT INTO user_table (
         SELECT distinct userid, firstname, lastname, gender, level
         FROM staging_events
-        WHERE page='NextSong'
+        )
 """)
 
 song_table_insert = ("""
+    INSERT INTO song (
         SELECT distinct song_id, title, artist_id, year, duration
         FROM staging_songs
+        )
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artist(
         SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
         FROM staging_songs
+        )
 """)
 
 time_table_insert = ("""
+    INSERT INTO time(
         SELECT  start_time
         ,       extract(hour from start_time) as hour
         ,       extract(day from start_time) as day
@@ -184,6 +192,7 @@ time_table_insert = ("""
         ,       extract(year from start_time) as year
         ,       extract(dayofweek from start_time) as dayofweek
         FROM    songplay
+        )
 """)
 
 # QUERY LISTS
