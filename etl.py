c486ec9ad2dba7d68ea9729a49a24a765d6ddb80
RobinVI97 <168171219+RobinVI97@users.noreply.github.com>
2024-08-24 22:49:08 +0200

    Update etl.py

--- etl.py
+++ etl.py
@@ -2,19 +2,19 @@ import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
-
+"""Extract staging tables from S3 to Redshift Landing."""
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
-
+"""Populate tables with table for final tables for analytical purposes."""
 def insert_tables(cur, conn):
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
-
+"""Connect with Redshift Data Warehouse and call the functions."""
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
